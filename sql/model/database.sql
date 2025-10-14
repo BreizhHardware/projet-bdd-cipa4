df6eb33690de2c4093bce09053993894acd543b9
Félix MARQUET <72651575+BreizhHardware@users.noreply.github.com>
2025-10-14 08:53:35 +0200

    refactor: make fields nullable in database schema

--- sql/model/database.sql
+++ sql/model/database.sql
@@ -8,7 +8,7 @@
 -- ----------------------------
 CREATE TABLE Marque (
                         Id_marque uuid NOT NULL DEFAULT gen_random_uuid(),
-                        marque VARCHAR(255) NOT NULL,
+                        marque VARCHAR(255),
                         CONSTRAINT Marque_PK PRIMARY KEY (Id_marque)
 );
 
@@ -19,7 +19,7 @@ CREATE TABLE Marque (
 CREATE TABLE Region (
                         region_code uuid NOT NULL DEFAULT gen_random_uuid(),
                         nom VARCHAR(255) NOT NULL,
-                        pays VARCHAR(5) NOT NULL,
+                        pays VARCHAR(255),
                         CONSTRAINT Region_PK PRIMARY KEY (region_code)
 );
 
@@ -29,7 +29,7 @@ CREATE TABLE Region (
 -- ----------------------------
 CREATE TABLE Installateur (
                               Id_installateur uuid NOT NULL DEFAULT gen_random_uuid(),
-                              installateur TEXT NOT NULL,
+                              installateur TEXT,
                               est_valide BOOLEAN NOT NULL,
                               CONSTRAINT Installateur_PK PRIMARY KEY (Id_installateur)
 );
@@ -41,9 +41,9 @@ CREATE TABLE Installateur (
 CREATE TABLE Panneau (
                          Id_panneau uuid NOT NULL DEFAULT gen_random_uuid(),
                          modele VARCHAR(255),
-                         Id_marque uuid NOT NULL,
+                         Id_marque uuid,
                          CONSTRAINT Panneau_PK PRIMARY KEY (Id_panneau),
-                         CONSTRAINT Panneau_Id_marque_FK FOREIGN KEY (Id_marque) REFERENCES Marque (Id_marque)
+                         CONSTRAINT Panneau_Id_marque_FK FOREIGN KEY (Id_marque) REFERENCES Marque (Id_marque) ON DELETE CASCADE
 );
 
 
@@ -53,9 +53,9 @@ CREATE TABLE Panneau (
 CREATE TABLE Onduleur (
                           Id_onduleur uuid NOT NULL DEFAULT gen_random_uuid(),
                           modele VARCHAR(255),
-                          Id_marque uuid NOT NULL,
+                          Id_marque uuid,
                           CONSTRAINT Onduleur_PK PRIMARY KEY (Id_onduleur),
-                          CONSTRAINT Onduleur_Id_marque_FK FOREIGN KEY (Id_marque) REFERENCES Marque (Id_marque)
+                          CONSTRAINT Onduleur_Id_marque_FK FOREIGN KEY (Id_marque) REFERENCES Marque (Id_marque) ON DELETE CASCADE
 );
 
 
@@ -67,7 +67,7 @@ CREATE TABLE Departement (
                              nom VARCHAR(255) NOT NULL,
                              region_code uuid,
                              CONSTRAINT Departement_PK PRIMARY KEY (departement_code),
-                             CONSTRAINT Departement_region_code_FK FOREIGN KEY (region_code) REFERENCES Region (region_code)
+                             CONSTRAINT Departement_region_code_FK FOREIGN KEY (region_code) REFERENCES Region (region_code) ON DELETE CASCADE
 );
 
 
@@ -75,13 +75,14 @@ CREATE TABLE Departement (
 -- Table: Localisation
 -- ----------------------------
 CREATE TABLE Localisation (
-                              code_Insee CHAR(5) NOT NULL,
+                              Id_ville uuid NOT NULL DEFAULT gen_random_uuid(),
+                              code_Insee CHAR(5),
                               ville VARCHAR(255) NOT NULL,
-                              code_postal CHAR(5) NOT NULL,
-                              population INT NOT NULL,
+                              code_postal CHAR(5),
+                              population INT,
                               departement_code uuid,
-                              CONSTRAINT Localisation_PK PRIMARY KEY (code_Insee),
-                              CONSTRAINT Localisation_departement_code_FK FOREIGN KEY (departement_code) REFERENCES Departement (departement_code)
+                              CONSTRAINT Localisation_PK PRIMARY KEY (Id_ville),
+                              CONSTRAINT Localisation_departement_code_FK FOREIGN KEY (departement_code) REFERENCES Departement (departement_code) ON DELETE CASCADE
 );
 
 
@@ -90,25 +91,25 @@ CREATE TABLE Localisation (
 -- ----------------------------
 CREATE TABLE Installation (
                               Id_installation uuid NOT NULL DEFAULT gen_random_uuid(),
-                              nb_panneaux INT NOT NULL,
-                              nb_onduleurs INT NOT NULL,
-                              date_installation DATE NOT NULL,
-                              production INT NOT NULL,
-                              puissance_crete INT NOT NULL,
-                              orientation INT NOT NULL,
-                              orientation_optimum INT NOT NULL,
-                              pente INT NOT NULL,
-                              pente_optimum INT NOT NULL,
-                              surface INT NOT NULL,
-                              latitude VARCHAR(6) NOT NULL,
-                              longitude VARCHAR(6) NOT NULL,
-                              code_Insee CHAR(5),
+                              nb_panneaux INT,
+                              nb_onduleurs INT,
+                              date_installation DATE,
+                              production INT,
+                              puissance_crete INT,
+                              orientation INT,
+                              orientation_optimum INT,
+                              pente INT,
+                              pente_optimum INT,
+                              surface INT,
+                              latitude VARCHAR(6),
+                              longitude VARCHAR(6),
+                              Id_ville uuid,
                               Id_installateur uuid,
                               Id_panneau uuid,
                               Id_onduleur uuid,
                               CONSTRAINT Installation_PK PRIMARY KEY (Id_installation),
-                              CONSTRAINT Installation_code_Insee_FK FOREIGN KEY (code_Insee) REFERENCES Localisation (code_Insee),
-                              CONSTRAINT Installation_Id_installateur_FK FOREIGN KEY (Id_installateur) REFERENCES Installateur (Id_installateur),
-                              CONSTRAINT Installation_Id_panneau_FK FOREIGN KEY (Id_panneau) REFERENCES Panneau (Id_panneau),
-                              CONSTRAINT Installation_Id_onduleur_FK FOREIGN KEY (Id_onduleur) REFERENCES Onduleur (Id_onduleur)
+                              CONSTRAINT Installation_Id_ville_FK FOREIGN KEY (Id_ville) REFERENCES Localisation (Id_ville) ON DELETE CASCADE,
+                              CONSTRAINT Installation_Id_installateur_FK FOREIGN KEY (Id_installateur) REFERENCES Installateur (Id_installateur) ON DELETE SET NULL,
+                              CONSTRAINT Installation_Id_panneau_FK FOREIGN KEY (Id_panneau) REFERENCES Panneau (Id_panneau) ON DELETE SET NULL,
+                              CONSTRAINT Installation_Id_onduleur_FK FOREIGN KEY (Id_onduleur) REFERENCES Onduleur (Id_onduleur) ON DELETE SET NULL
 );
